a403ddb8a0ba12ba04fdf6013a6e77782709f73f
Alekseii <1914.ac@gmail.com>
2022-05-31 18:44:59 +0300

    [index.js] create new file for export UI components

---
+++ src/components/UI/index.js
@@ -0,0 +1,9 @@
+import FinButton from "@/components/UI/FinButton.vue";
+import FinInput from "@/components/UI/FinInput.vue";
+import FinSelect from "@/components/UI/FinSelect.vue";
+
+export default {
+  FinButton,
+  FinInput,
+  FinSelect,
+};
