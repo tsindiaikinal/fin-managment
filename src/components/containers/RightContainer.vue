1e33e65108ea22d9316887bb1ba34ed1d7215d9b
Alekseii <1914.ac@gmail.com>
2022-05-31 18:23:47 +0300

    [scss] changed stules in RightContainer.vue

--- src/components/containers/RightContainer.vue
+++ src/components/containers/RightContainer.vue
@@ -10,9 +10,10 @@ export default {};
 
 <style lang="scss" scoped>
 .right-container {
+  min-width: 270px;
   flex-shrink: 1;
   width: 20%;
-  height: 100vh;
-  border: 1px solid #b8aeae;
+  // height: 100vh;
+  // border: 1px solid #b8aeae;
 }
 </style>
