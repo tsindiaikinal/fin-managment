737dded6d9cc00b1efd5579d56ca470143f53706
Alekseii <1914.ac@gmail.com>
2022-05-31 18:14:10 +0300

    [scss] changed default style in FitButton component

--- src/components/UI/FinButton.vue
+++ src/components/UI/FinButton.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <button class="fin-btn">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fin-btn {
+  display: block;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
