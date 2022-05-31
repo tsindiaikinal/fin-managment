c4d0ce81b0bf089d2d25d201f465f229d8bd5d31
Alekseii <1914.ac@gmail.com>
2022-05-31 17:44:32 +0300

    [scss] style of the ProfileElem.vue component

--- src/components/ProfileElem.vue
+++ src/components/ProfileElem.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="profile-elem">
-    <img src="avatar" alt="avatar" />
-    <p>username</p>
-    <p>company</p>
+  <div class="profile">
+    <div class="profile__img">
+      <img src="../assets/avatar.jpg" alt="avatar" />
+    </div>
+    <div class="profile__about">
+      <span class="profile__user">{{ username }}</span>
+      <span class="profile__user">{{ company }}</span>
+    </div>
+    <div class="profile__btns">
+      <fin-button class="profile__btn">
+        <mdicon name="tune" size="16" />
+      </fin-button>
+      <fin-button class="profile__btn">
+        <mdicon name="help-circle-outline" size="16" />
+      </fin-button>
+    </div>
   </div>
 </template>
 
@@ -11,11 +23,52 @@ export default {
   data() {
     return {
       username: "Geis",
-      avatar: "",
+      avatar: "../assets/avatar.jpg",
       company: "Techconnect",
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 25px;
+
+  border-radius: 8px;
+  background-color: #eee;
+  &__about {
+    padding: 5px 10px;
+  }
+  &__img {
+    width: 40px;
+    height: 40px;
+    img {
+      width: 100%;
+
+      border-radius: 100%;
+    }
+  }
+
+  &__user {
+    display: inline-block;
+
+    width: 100%;
+    margin: 3px 0;
+
+    text-align: left;
+  }
+
+  &__btn {
+    border-radius: 100%;
+    padding: 7px;
+    &:hover {
+      color: #1cb3e0;
+      background-color: #fff;
+    }
+  }
+}
+</style>
