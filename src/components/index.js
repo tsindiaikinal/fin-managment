a1580e89abf0f2745b8e036ee027225a13343e86
Alekseii <1914.ac@gmail.com>
2022-05-31 18:28:34 +0300

    [js] added new components for registration

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import DashBoard from "@/components/DashBoard.vue";
 import DiagramMove from "@/components/DiagramMove.vue";
 import ProfileElem from "@/components/ProfileElem.vue";
 import SideMenu from "@/components/SideMenu.vue";
+import TotalElem from "@/components/TotalElem.vue";
 
 export default {
   DashBoard,
   DiagramMove,
   ProfileElem,
   SideMenu,
+  TotalElem,
 };
