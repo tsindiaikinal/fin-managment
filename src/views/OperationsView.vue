a285529f529a2eca3796cbd3a9b0f4f370abd984
Alekseii <1914.ac@gmail.com>
2022-06-09 22:26:37 +0300

    [js, css] building a OperationView page. Added top components and test of card

--- src/views/OperationsView.vue
+++ src/views/OperationsView.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="operations">
+  <div class="operation">
     <top-container :title="mainTitle"></top-container>
-    <div class="operations__panel"></div>
-    <div class="operations__diagram"></div>
+    <div class="operation__panel">
+      <month-select class="panel__month-elem"></month-select>
+      <div class="panel__search-elem">
+        <search-elem
+          textholder="по проекту, контрагенту или счёту"
+          v-model="val"
+        ></search-elem>
+      </div>
+      <div class="panel__select-elem">
+        <fin-select></fin-select>
+      </div>
+    </div>
+    <div class="operation__diagram">
+      <diagram-move></diagram-move>
+    </div>
+    <div class="operation__cards">
+      <operation-card></operation-card>
+    </div>
   </div>
 </template>
 
@@ -12,20 +28,40 @@ export default {
   data() {
     return {
       mainTitle: "Операции",
+      val: "",
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-// .operations {
-.operations {
+.operation {
   &__panel {
-    padding: 10px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 17px 26px 0;
+  }
+  &__diagram {
+    padding-right: 17px;
+  }
+  &__cards {
+    overflow: auto;
+  }
+}
+
+.panel {
+  &__month-elem {
+    width: 20%;
   }
 
-  &__diagram {
-    padding: 25px;
+  &__search-elem {
+    width: 40%;
+  }
+
+  &__select-elem {
+    width: 30%;
   }
 }
 </style>
