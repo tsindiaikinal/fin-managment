cb28280b733b2128f82d220cce027db74432130f
Alekseii <1914.ac@gmail.com>
2022-06-09 22:10:56 +0300

    [UI/index] add in list new UI components

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,9 +1,13 @@
 import FinButton from "@/components/UI/FinButton.vue";
 import FinInput from "@/components/UI/FinInput.vue";
 import FinSelect from "@/components/UI/FinSelect.vue";
+import SearchElem from "@/components/UI/SearchElem.vue";
+import MonthSelect from "@/components/UI/MonthSelect.vue";
 
 export default {
   FinButton,
   FinInput,
   FinSelect,
+  SearchElem,
+  MonthSelect,
 };
