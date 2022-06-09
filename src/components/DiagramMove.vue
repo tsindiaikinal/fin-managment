c8e876cecfe8f2899b1cee224ad91fe21e2cf59c
Alekseii <1914.ac@gmail.com>
2022-06-09 20:49:32 +0300

    [css] add styles for DiagramMove component

--- src/components/DiagramMove.vue
+++ src/components/DiagramMove.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div class="diagram">
+    <img src="@/assets/money-move.jpg" alt="Money move" />
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.diagram {
+  img {
+    width: 100%;
+  }
+}
+</style>
