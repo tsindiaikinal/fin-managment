0bc07f8892ee93a35ff16a4f84277ba83df87f1f
Alekseii <1914.ac@gmail.com>
2022-06-09 22:00:40 +0300

    [js, css] name and styles for component FinInput

--- src/components/UI/FinInput.vue
+++ src/components/UI/FinInput.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <input
+    class="fin-input"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
 </template>
 
 <script>
-export default {};
+export default {
+  name: "finInput",
+  props: ["value"],
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fin-input {
+  width: 100%;
+  padding: 12px 15px;
+  border: 1px solid #e5e5e5;
+  border-radius: 8px;
+}
+</style>
