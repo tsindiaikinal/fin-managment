d79a157b5d43a664a2497783ee18ba5ae7c1188a
Alekseii <1914.ac@gmail.com>
2022-06-09 22:17:11 +0300

    [main.js] included custom derectives to app

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import mdiVue from "mdi-vue/v2";
 import * as mdijs from "@mdi/js";
 import router from "./router";
 import store from "./store";
-import containers from "@/components/containers";
+import containers from "@/containers";
 import components from "@/components";
 import componentsUI from "@/components/UI";
+import directives from "@/directives";
 
 for (const container in containers) {
   Vue.component(container, containers[container]);
@@ -16,6 +17,10 @@ for (const component in components) {
 }
 for (const componentUI in componentsUI) {
   Vue.component(componentUI, componentsUI[componentUI]);
+}
+
+for (const directive in directives) {
+  Vue.directive(directives[directive].name, directives[directive]);
 }
 
 Vue.use(mdiVue, {
